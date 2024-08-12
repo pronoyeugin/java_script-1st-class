64e7847c7e7a6333ada3b338ad82c4860ba11d81
Pronoy Eugin Khalko <130691366+pronoyeugin@users.noreply.github.com>
2024-08-12 23:46:26 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,30 @@
+function readmore(){
+	document.getElementById('s_text').innerHTML="SSB-619 Full stack Web Development";
+}
+function readmore2(){
+	document.getElementById('s_text2').innerHTML="SSB-619 Full stack Web Development";
+}
+function readmore3(){
+	document.getElementById('s_text3').innerHTML="SSB-619 Full stack web development";
+}
+function readmore4(){
+	document.getElementById('s_text4').innerHTML="SSB-619 Full Stack Web Development";
+}
+function readmore5(){
+	document.getElementById('s_text5').innerHTML="SSB-619 Full stack web Development";
+}
+function readmore6(){
+	document.getElementById('s_text6').innerHTML="SSB-619 Full stack web development";
+}
+function readmore7(){
+	document.getElementById('s_text7').innerHTML="SSB-619 Full stack web development";
+}
+function readmore8(){
+	document.getElementById('s_text8').innerHTML="SSB-619 Full stack web Development";
+}
+function readmore9(){
+	document.getElementById('s_text9').innerHTML="SSB-619 Full stack Web Development";
+}
+function readmore10(){
+	document.getElementById('s_text10').innerHTML="SSB-619 Full Stack Web Development";
+}
